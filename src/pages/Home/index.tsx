79e01eabb81c0a3a96c3eebdc1eaa24217d14d55
Jhonatan Oliveira <jhonatandev0@gmail.com>
2021-09-11 21:37:02 -0300

    Criando page consultaCep

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,12 +8,12 @@ function Home() {
             <NavBar />
             <div className="container">
                 <div className="jumbotron">
-                    <h1 className="display-4">DSVendas</h1>
-                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <h1 className="display-4">Consulta Cep</h1>
+                    <p className="lead">Consulte seu cep no botão abaixo:</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-                    <Link className="btn btn-primary btn-lg" to="/dashboard">
-                        Acessar dashboard
+                    <p>Esta aplicação consiste em exibir o resultado de sua busca a partir da utilização de uma API externa.</p>
+                    <Link className="btn btn-primary btn-lg" to="/consultacep">
+                        Acessar
                     </Link>
                 </div>
             </div>
