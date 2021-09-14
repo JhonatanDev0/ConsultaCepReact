2297a3b4f83c6c194e52c6c68fe1a392e834953e
Jhonatan Oliveira <jhonatandev0@gmail.com>
2021-09-13 21:47:06 -0300

    CSS Pronto

--- src/pages/Consultacep/index.tsx
+++ src/pages/Consultacep/index.tsx
@@ -5,19 +5,32 @@ import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function consultaCep() {
+    const myStyle = {
+        width: '35%',
+        marginLeft: '630px',
+        marginTop: '-450px',
+    }
+    const myStyle2 = {
+        marginTop: '50px'
+
+    }
     return (
         <>
             <NavBar />
-            <div className="container">
+            <div className="container" style={myStyle2}>
                 <img className="py-5" src={BuscaImg} />
             </div>
             <div className="container">
-                <form>
+                <div style={myStyle}>
                     <label className="fw-bold">Digite seu CEP:</label>
                     <br />
-                    <input type="text" className="form-control" />
-                    <button className="btn btn-warning" type ="button"><img src={Search} /></button>
-                </form>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" />
+                        <div className="input-group-append">
+                            <button className="btn btn-warning" type="button"><img src={Search} /></button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
